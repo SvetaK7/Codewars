6c90ea64004c6094f1bd045d2b0668acdbbc3505
SvetaK7 <atevs7@inbox.ru>
2022-02-19 14:06:49 +0300

    Count the number of digits

---
+++ 18_count_Digits/script18.js
@@ -0,0 +1,11 @@
+
+function paintLetterboxes(start, end){
+   let res = Array(10).fill(0);
+   for (let i = start; i <= end; i++){
+      for (let number of (i + '')){
+         res[number]++;
+      }
+   }
+   return res;
+}
+console.log(paintLetterboxes(25, 27));
