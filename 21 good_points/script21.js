23827491d549bedcb7f561059696255283439186
SvetaK7 <atevs7@inbox.ru>
2022-04-15 20:54:07 +0300

    better point

---
+++ 21 good_points/script21.js
@@ -0,0 +1,11 @@
+function betterThanAverage(classPoints, yourPoints){
+
+let sum = classPoints.map((num) => x = x + num, x = 0).reverse()[0];
+let ball = sum / classPoints.length;
+
+return yourPoints > ball ? true : false;
+
+
+
+}
+console.log(betterThanAverage([4,3,9,7,2,1], 5));
