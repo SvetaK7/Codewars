e4dc955e95c226bf0dbe0b3e64d1feeeca158b38
SvetaK7 <atevs7@inbox.ru>
2023-01-05 21:56:26 +0300

    The Collatz Conjecture right

---
+++ 46_collatz/script46.js
@@ -0,0 +1,17 @@
+function collatz(n) {
+    let arr = [n];
+    while (n !== 1){
+        if (n % 2 === 0){
+            n = n /2;
+        }
+        else {
+            n = (n * 3) + 1;
+        }
+        arr.push(n);
+    }
+    return arr.length;
+}
+
+console.log(collatz(20))
+
+
